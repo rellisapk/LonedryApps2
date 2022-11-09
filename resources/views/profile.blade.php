2fd166bb192014c241e0074c3cb5a8b424a19ac6
unknown <victoriatasia@gmail.com>
2022-11-09 14:28:07 +0700

    profile ++

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -16,75 +16,75 @@
         <div class="col-8 mx-auto">
             <div class="card">
                 <div class="card-body">
-            <h3>Profile {{ $user->name }}</h3>
-            <div class="card">
-                <div class="card-header">
-                    <div class="row">
-                        <div class="col-6"><b>Biodata Diri</b></div>
-                        <div class="col-6 not-selected"><a href="/riwayat/{{Auth::user()->id}}"><b>Riwayat Pesanan</b></a></div>
+                    <h3>Profile {{ $user->name }}</h3>
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-6"><b>Biodata Diri</b></div>
+                                <div class="col-6 not-selected"><a href="/riwayat/{{Auth::user()->id}}"><b>Riwayat Pesanan</b></a></div>
+                            </div>
+                        </div>
+        
+                        <div class="card-body">
+                            <b>Profil Saya</b>
+                            <p>Kelola informasi profil Anda untuk mengontrol, melindungi dan mengamankan akun</p>
+                            <hr>
+                            <b>Ubah Biodata Diri</b>
+                            <br>
+                            <form action="{{ route('profile.update', Auth::user()->id) }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method("PUT")
+        
+                            <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input value="{{ $user->name }}" name="name" id="name" type="text" class="form-control" placeholder="Masukkan nama">
+                                    <p class="text-danger">{{ $errors->first("name") }}</p>
+                                </div>
+        
+                                <div class="form-group">
+                                    <label for="gender">Jenis Kelamin</label>
+                                    <select name="gender" class="form-control" id="gender">
+                                        <option value="0" {{ $user->gender == 0 ? "selected" : "" }}>Laki-Laki</option>
+                                        <option value="1" {{ $user->gender == 1 ? "selected" : "" }}>Perempuan</option>
+                                    </select>
+                                    <p class="text-danger">{{ $errors->first("gender") }}</p>
+                                </div>
+        
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input value="{{ $user->email }}" name="email" id="email" type="email" class="form-control" placeholder="Masukkan email">
+                                    <p class="text-danger">{{ $errors->first("email") }}</p>
+                                </div>
+        
+                                <div class="form-group">
+                                    <label for="address">Address</label>
+                                    <textarea name="address" id="address" type="text" class="form-control" placeholder="Masukkan address">{{ $user->address }}</textarea>
+                                    <p class="text-danger">{{ $errors->first("address") }}</p>
+                                </div>
+        
+                                <div class="form-group">
+                                    <label for="birth">Birth</label>
+                                    <input value="{{ $user->birth }}" name="birth" id="birth" type="date" class="form-control" placeholder="Masukkan birth">
+                                    <p class="text-danger">{{ $errors->first("birth") }}</p>
+                                </div>
+        
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input value="" name="password" id="password" type="password" class="form-control" placeholder="Masukkan password">
+                                    <p class="text-danger">{{ $errors->first("password") }}</p>
+                                </div>
+        
+                                <div class="form-group">
+                                    <label for="phone">No Telepon</label>
+                                    <input value="{{ $user->phone }}" name="phone" id="phone" type="number" class="form-control" placeholder="Masukkan nomor telepon">
+                                    <p class="text-danger">{{ $errors->first("phone") }}</p>
+                                </div>
+        
+                                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-
-                <div class="card-body">
-                    <b>Profil Saya</b>
-                    <p>Kelola informasi profil Anda untuk mengontrol, melindungi dan mengamankan akun</p>
-                    <hr>
-                    <b>Ubah Biodata Diri</b>
-                    <br>
-                    <form action="{{ route('profile.update', Auth::user()->id) }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    @method("PUT")
-
-                    <div class="form-group">
-                            <label for="name">Name</label>
-                            <input value="{{ $user->name }}" name="name" id="name" type="text" class="form-control" placeholder="Masukkan nama">
-                            <p class="text-danger">{{ $errors->first("name") }}</p>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="gender">Jenis Kelamin</label>
-                            <select name="gender" class="form-control" id="gender">
-                                <option value="0" {{ $user->gender == 0 ? "selected" : "" }}>Laki-Laki</option>
-                                <option value="1" {{ $user->gender == 1 ? "selected" : "" }}>Perempuan</option>
-                            </select>
-                            <p class="text-danger">{{ $errors->first("gender") }}</p>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input value="{{ $user->email }}" name="email" id="email" type="email" class="form-control" placeholder="Masukkan email">
-                            <p class="text-danger">{{ $errors->first("email") }}</p>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="address">Address</label>
-                            <textarea name="address" id="address" type="text" class="form-control" placeholder="Masukkan address">{{ $user->address }}</textarea>
-                            <p class="text-danger">{{ $errors->first("address") }}</p>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="birth">Birth</label>
-                            <input value="{{ $user->birth }}" name="birth" id="birth" type="date" class="form-control" placeholder="Masukkan birth">
-                            <p class="text-danger">{{ $errors->first("birth") }}</p>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input value="" name="password" id="password" type="password" class="form-control" placeholder="Masukkan password">
-                            <p class="text-danger">{{ $errors->first("password") }}</p>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="phone">No Telepon</label>
-                            <input value="{{ $user->phone }}" name="phone" id="phone" type="number" class="form-control" placeholder="Masukkan nomor telepon">
-                            <p class="text-danger">{{ $errors->first("phone") }}</p>
-                        </div>
-
-                        <button type="submit" class="btn btn-primary btn-lg">Submit</button>
-                    </form>
-                </div>
-            </div>
-            </div>
             </div>
         </div>
     </div>
